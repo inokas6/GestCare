6fb14a8e6aa6d1c636e954e83499e0d9a1adb2ed
inokas6 <ineslaramiranda6@gmail.com>
2025-05-04 13:54:59 +0100

    mais modelo 3d

--- app/componets/Modelo3D/modelo3d.js
+++ app/componets/Modelo3D/modelo3d.js
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 
 function Modelo() {
-  const gltf = useGLTF('/modelos3d/semana1.glb');
+  const gltf = useGLTF('/modelos3d/semana6.glb');
   return React.createElement('primitive', { object: gltf.scene });
 }
 
@@ -11,7 +11,7 @@ function Modelo3D() {
   return React.createElement(
     Canvas,
     { style: { height: '500px' } },
-    React.createElement('ambientLight', { intensity: 0.5 }),
+    React.createElement('ambientLight', { intensity: 0.6 }),
     React.createElement('directionalLight', { position: [0, 5, 5] }),
     React.createElement(Suspense, { fallback: null },
       React.createElement(Modelo)
